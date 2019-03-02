75202f23046f1d44b10b8bef919b12996b48ab20
Jason T <jasontanner328@gmail.com>
2019-03-02 13:50:52 -0500

    Fixing code

--- lib/generators/graphql_client/install_generator.rb
+++ lib/generators/graphql_client/install_generator.rb
@@ -15,12 +15,7 @@ module GraphqlClient
     def install
       template("initializer.erb", "config/initializers/graphql_client.rb")
 
-      inject_into_file 'app/controllers/application_controller.rb', after: "class ApplicationController < ActionController::Base\n" do <<-'RUBY'
-                                                                                                                                         def graphql_context
-                                                                                                                                           # Add your context here
-                                                                                                                                           {}
-                                                                                                                                         end
-                                                                                                                                       RUBY
+      inject_into_file 'app/controllers/application_controller.rb', after: "class ApplicationController < ActionController::Base\n" do 'def graphql_context; # Add your context here; {}; end'
     end
 
     private
